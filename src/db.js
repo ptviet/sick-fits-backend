8c8c3aee45fdcdd0f10c0ed6f2022e762f995be7
Steven Phan <ptviet@live.com>
2018-12-28 13:58:43 +1100

    Setup DB, GraphQL Yoga Server

---
+++ src/db.js
@@ -0,0 +1,11 @@
+// Connect to the remote Prisma DB and Enable the ability to query with JS
+const { Prisma } = require('prisma-binding');
+
+const db = new Prisma({
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-// let's go!
+require('dotenv').config({ path: 'variable.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is now running on: localhost:${deets.port}`);
+  }
+);
+
+// Use Express middleware to handle cookies (JWT)
+
+// Use Express middleware to populate current user
