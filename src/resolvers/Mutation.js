e9d79deb958bc2a02dc14c2c341d814a5acff037
Steven Phan <ptviet@live.com>
2018-12-28 21:47:57 +1100

    modify args

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,14 @@
-const Mutations = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // Check if logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args }
+      },
+      info
+    );
+    return item;
+  }
+};
 
-module.exports = Mutations;
+module.exports = Mutation;
